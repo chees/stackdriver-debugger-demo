ef6a943f4d536f3998f011f8d6f1af4de36c3972
Christiaan Hees <mail@chees.info>
2017-09-03 10:38:29 +0200

    Version 4

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! version 4')
 })
 
 app.get('/health', (req, res) => {
